1563fe7aaae836543ba07dd858c42e6abc4e50c3
Christian McCagherty <christianmccagherty164@gmail.com>
2025-06-02 17:02:59 +0100

    skip auth for login and signup

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,11 +1,13 @@
 class Api::AuthController < ApplicationController
+  skip_before_action :authorize_request, only: [ :login, :signup ]
+
   def login
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: user.id)
       render json: { token: token, user: user }, status: :ok
     else
-      render json: { error: 'Invalid credentials' }, status: :unauthorized
+      render json: { error: "Invalid credentials" }, status: :unauthorized
     end
   end
 
