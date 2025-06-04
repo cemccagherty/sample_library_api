e890af367b840e8eea5970e47a8623a41f90dd26
Christian McCagherty <christianmccagherty164@gmail.com>
2025-06-04 14:43:54 +0100

    test: added shoulda-matchers test for username uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password) }
+  it { should validate_uniqueness_of(:password) }
   it { should validate_length_of(:password).is_at_least(6) }
 
   it "is valid with valid attributes" do
